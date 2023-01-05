2ec7a2e968a4084693597d292ed9a106d312bf81
Hiro-Alter <brayan.mena@correounivalle.edu.co>
2023-01-04 20:07:05 -0500

    Configuracion inicial

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//Configuraciones
+app.set('port', 3000);
+app.set('views', path.join(__dirname + '/views'));
+app.set('view engine', 'ejs');
+
+//Rutas
+app.use(require('./routes'));
+
+
+//Archivos estaticos
+
+
+//Servidor escuchando
+app.listen(app.get('port'), () =>{
+    console.log('server on port', app.get('port'))
+});
